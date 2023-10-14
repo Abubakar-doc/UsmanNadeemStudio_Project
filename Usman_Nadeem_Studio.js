2c2318bac13c2a41f63dfa78263a23ad25c6bdb3
Abubakar <137390804+Abubakar-doc@users.noreply.github.com>
2023-10-14 22:17:41 +0500

    Pull for preloader

---
+++ Usman_Nadeem_Studio.js
@@ -0,0 +1,9 @@
+// function hidePreloader() {
+//     var loader = document.getElementById("preloader");
+//     loader.style.display = "none";
+//   }
+//   window.addEventListener('load', hidePreloader);
+  
+//   function removeFocus() {
+//     document.activeElement.blur();
+//   }
