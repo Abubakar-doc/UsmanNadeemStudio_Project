93d1829c9f8ad10d65a1a6539d37ba14627480e1
Abubakar <137390804+Abubakar-doc@users.noreply.github.com>
2023-10-25 11:30:55 +0500

    Delete Usman_Nadeem_Studio.js

--- Usman_Nadeem_Studio.js
+++
@@ -1,9 +0,0 @@
-// function hidePreloader() {
-//     var loader = document.getElementById("preloader");
-//     loader.style.display = "none";
-//   }
-//   window.addEventListener('load', hidePreloader);
-  
-//   function removeFocus() {
-//     document.activeElement.blur();
-//   }
