c6bfd573b212eb2e6b9e742a1c92cd6579159533
Abubakar <137390804+Abubakar-doc@users.noreply.github.com>
2023-11-22 08:48:23 +0500

    Update Usman_Nadeem_Studio.js

--- Usman_Nadeem_Studio.js
+++ Usman_Nadeem_Studio.js
@@ -1,7 +1,3 @@
-
-
-///
-
 function hidePreloader() {
   var loader = document.getElementById("preloader");
   loader.style.display = "none";
@@ -17,4 +13,44 @@ function removeFocus() {
 }
 
 ///
+
+let lastScrollTop = 0;
+const navbar = document.getElementById('desk_nav');
+const navbar_inside = document.getElementById('a');
+let navbarHeight = navbar.offsetHeight;
+const transparentRange = 50; // Adjust this value to define the range in pixels
+
+window.addEventListener('scroll', function() {
+  let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (currentScroll > lastScrollTop) {
+    // Scrolling down
+    navbar.style.position = 'relative';
+    navbar.style.transition = 'background-color 0.4s ease-in-out';
+    navbar.style.visibility = 'none';
+    navbar.style.boxShadow = 'none'; 
+  } else {
+    // Scrolling up
+    navbar.style.position = 'fixed';
+    navbar.style.borderBottomLeftRadius = '2rem';
+    navbar.style.borderBottomRightRadius = '2rem';
+    navbar.style.transition = 'background-color 0.4s ease-in-out';
+    navbar.style.visibility = 'visible';
+    navbar.style.boxShadow = '0px 4px 6px rgba(0, 0, 0, 0.1)';
+
+    if (currentScroll < transparentRange) {
+      navbar.style.backgroundColor = 'transparent';
+      navbar.style.boxShadow = 'none'; 
+    } else {
+      navbar.style.backgroundColor = '#f0f0f0';
+    }
+
+    navbar_inside.style.display = 'flex';
+    navbar_inside.style.justifyContent = 'space-between';
+  }
+
+  lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
+});
+
+
 
