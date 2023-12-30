5cffc93bb5e058a9174a1868642cd60923b8fa73
Abubakar <137390804+Abubakar-doc@users.noreply.github.com>
2023-12-31 02:33:27 +0500

    update

--- src/Usman_Nadeem_Studio.js
+++ src/Usman_Nadeem_Studio.js
@@ -263,3 +263,11 @@ window.addEventListener('scroll', function() {
 
 ///
 
+// small navigation  menu js
+function openNav() {
+  document.getElementById("myNav").classList.toggle("menu_width");
+  document
+      .querySelector(".custom_menu-btn")
+      .classList.toggle("menu_btn-style");
+
+}
